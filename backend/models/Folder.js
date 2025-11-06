27000d52c5f6f003f995742019990925f863126a
EsraaNasr92 <esraanasr706@gmail.com>
2025-11-06 12:49:46 +0200

    Solved: Fix folder model duplicate key issue

--- backend/models/Folder.js
+++ backend/models/Folder.js
@@ -1,10 +1,6 @@
 import mongoose from "mongoose";
 
 const folderSchema = new mongoose.Schema({
-    id:{
-        type: Number,
-        unique: true,
-    },
     title:{
         type: String,
         required: true,
