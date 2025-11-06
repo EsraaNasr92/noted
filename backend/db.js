c5da89a29549920156495d77084109eba4b4c3a5
EsraaNasr92 <esraanasr706@gmail.com>
2025-11-06 11:35:43 +0200

    Config: Vercel and MongoDB

--- backend/db.js
+++ backend/db.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose';
 
 const connectDB = async () => {
     try {
-        await mongoose.connect('mongodb://localhost:27017/noted_app');
+        await mongoose.connect('process.env.MONGO_URL');
         console.log('MongoDB Connected');
     } catch (error) {
         console.error('MongoDB connection error:', error);
